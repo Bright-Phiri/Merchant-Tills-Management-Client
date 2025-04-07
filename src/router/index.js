d64a3d6b8ea9424056bcfe30c23197a0dd8c2720
Bright Issah <bphiri.aki@gmail.com>
2025-04-07 10:22:13 +0200

    Add subscriptiuons view

--- src/router/index.js
+++ src/router/index.js
@@ -40,6 +40,14 @@ const router = createRouter({
           // which is lazy-loaded when the route is visited.
           component: () => import('../views/TerminalsView.vue'),
         },
+        {
+          path: '/subscriptions',
+          name: 'subscriptions',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('../views/SubscriptionsView.vue'),
+        },
       ],
     },
   ],
---
+++ src/views/SubscriptionsView.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+const fetchSubscriptionsLoading = ref(true)
+const headers = [
+  {
+    align: 'start',
+    key: 'owner',
+    sortable: false,
+    title: 'Owner',
+  },
+  { key: 'taxpayer', title: 'Client Name' },
+  { key: 'start_date', title: 'Start Date' },
+  { key: 'end_date', title: 'End Date' },
+]
+</script>
+<template>
+  <div class="Clients">
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title class="d-flex justify-space-between">
+            <span>Subscriptions</span>
+            <v-col cols="3">
+              <v-text-field
+                prepend-inner-icon="mdi-magnify"
+                clearable
+                placeholder="Search subscription"
+                variant="outlined"
+                density="compact"
+              ></v-text-field>
+            </v-col>
+          </v-card-title>
+          <v-card-text>
+            <v-data-table
+              class="elevation-1"
+              :headers="headers"
+              :search="search"
+              :loading="fetchSubscriptionsLoading"
+              loading-text="Loading subscriptions..."
+            ></v-data-table>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
