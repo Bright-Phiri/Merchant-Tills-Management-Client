f1f4e3af30fc5687b92ec9f137099f5013766016
Bright Issah <bphiri.aki@gmail.com>
2025-04-23 07:29:29 +0200

    WIP

--- src/lib/cable.js
+++ src/lib/cable.js
@@ -1,5 +1,5 @@
 import { createConsumer } from '@rails/actioncable'
 
-const cable = createConsumer('ws://localhost:3000/websocket') // or /cable if you're using the default
+const cable = createConsumer('ws://localhost:3000/websocket')
 
 export default cable
