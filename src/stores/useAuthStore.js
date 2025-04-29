f23e8acfabdc3e2ad16bf364899344bedf8cd9cd
Bright Issah <bphiri.aki@gmail.com>
2025-04-29 16:44:01 +0200

    WIP

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -1,29 +1,37 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    token: null,
-    user_name: null,
-    role: null,
-    isUserLoggedIn: false,
-  }),
-  actions: {
-    setToken(token) {
-      this.token = token
-      this.isUserLoggedIn = !!token
-    },
-    setUserName(name) {
-      this.user_name = name
-    },
-    setUserRole(role) {
-      this.role = role
-    },
-    logout() {
-      this.token = null
-      this.user_name = null
-      this.role = null
-      this.isUserLoggedIn = false
-    },
+export const useAuthStore = defineStore(
+  'auth',
+  () => {
+    const token = ref(null)
+    const user_name = ref(null)
+    const role = ref(null)
+    const isUserLoggedIn = ref(false)
+
+    function setToken(new_token) {
+      token.value = new_token
+      isUserLoggedIn.value = true
+    }
+
+    function setUserName(name) {
+      user_name.value = name
+    }
+
+    function setUserRole(userRole) {
+      role.value = userRole
+    }
+
+    function logout() {
+      token.value = null
+      user_name.value = null
+      role.value = null
+      isUserLoggedIn.value = false
+    }
+
+    return { token, user_name, role, isUserLoggedIn, setToken, setUserName, setUserRole, logout }
   },
-  persist: true,
-})
+  {
+    persist: true,
+  },
+)
